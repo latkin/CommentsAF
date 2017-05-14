2c69bf32e3585cf7c5dacec56851f93963ff9fd2
Lincoln Atkinson <latkin@users.noreply.github.com>
2017-05-13 17:17:45 -0700

    More refactoring

---
+++ CommentsAF/commentstorage.fs
@@ -0,0 +1,45 @@
+﻿namespace CommentsAF
+
+open System
+open Microsoft.WindowsAzure.Storage
+open Microsoft.WindowsAzure.Storage.Table
+
+type Comment =
+    { time : DateTimeOffset
+      name : string
+      comment : string }
+
+module CommentStorage =
+    type StorageSettings =
+        { StorageConnectionString : string
+          TableName : string } with
+
+        static member load () = 
+            { StorageConnectionString =
+                Environment.GetEnvironmentVariable("APPSETTING_comments_connectionstring", EnvironmentVariableTarget.Process)
+              TableName =
+                Environment.GetEnvironmentVariable("APPSETTING_comments_tablename", EnvironmentVariableTarget.Process) }
+
+    type CommentRow() =
+        inherit TableEntity()
+        member val Name = "" with get,set
+        member val Comment = "" with get,set
+
+    type CommentStorage(settings) =
+        let table = 
+                CloudStorageAccount.Parse(settings.StorageConnectionString)
+                    .CreateCloudTableClient()
+                    .GetTableReference(settings.TableName)
+
+        let commentFromRow (row: CommentRow) =
+            { time = row.Timestamp
+              name = row.Name
+              comment = row.Comment }
+
+        member __.GetCommentsForPost(postId) =
+            let postKey = sprintf "post-%s" postId
+            let q =
+                TableQuery<CommentRow>()
+                    .Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, postKey))
+
+            table.ExecuteQuery(q) |> Seq.map commentFromRow |> Array.ofSeq
---
+++ CommentsAF/run.fsx
@@ -0,0 +1,40 @@
+﻿#if VS
+module run
+#else
+#r "System.Net.Http"
+#r "Newtonsoft.Json"
+#r "Microsoft.WindowsAzure.Storage"
+#load "commentstorage.fs"
+#endif
+
+open CommentsAF.CommentStorage
+open System.Collections.Generic
+open System.Net
+open System.Net.Http
+open Microsoft.Azure.WebJobs.Host
+open Newtonsoft.Json
+
+let Run(req: HttpRequestMessage, log: TraceWriter) =
+    async {
+    try
+        let postIdOpt =
+            req.GetQueryNameValuePairs()
+            |> Seq.tryFind (fun q -> q.Key = "postid")
+            |> Option.map (fun kvp -> kvp.Value)
+    
+        match postIdOpt with
+        | None -> return req.CreateErrorResponse(HttpStatusCode.BadRequest, "postid parameter required")
+        | Some(postId) ->
+            let storage = CommentStorage(StorageSettings.load())
+            let comments = storage.GetCommentsForPost(postId)
+
+            log.Info(sprintf "Loaded %d comments for post %s" comments.Length postId)
+
+            let resp = req.CreateResponse(HttpStatusCode.OK)
+            resp.Content <- new StringContent(JsonConvert.SerializeObject(comments))
+            return resp
+    with
+    | exn ->
+        log.Error("Unknown error getting comments", exn)
+        return req.CreateErrorResponse(HttpStatusCode.InternalServerError, "Unknown error")
+    } |> Async.RunSynchronously
